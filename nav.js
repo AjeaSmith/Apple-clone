4a58c492831bf7ca801e1a7fd87fcf4f34e1fc34
Ajea Smith <smithajea@rocketmail.com>
2022-04-17 03:28:53 -0400

    finished responsive nav using javascript

--- nav.js
+++ nav.js
@@ -1,1 +1,5 @@
-const navMenuButton = document.querySelector('.')
+const menuButton = document.querySelector('#menu-btn');
+const menu = document.querySelector('.mobile-menu');
+addEventListener('click', () => {
+	menu.style.display = menu.style.display !== 'block' ? 'block' : 'none';
+});
